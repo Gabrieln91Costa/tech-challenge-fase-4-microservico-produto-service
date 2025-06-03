1fa414ee4d74c3fc2dd966fac81b4b5dbc025b89
Gabriel <gabrieln91@hotmail.com>
2025-06-03 00:34:23 -0300

    Subindo Arquivos!!

--- produto-service/src/main/java/com/microservico/produtoservice/infrastructure/controller/ProdutoController.java
+++ produto-service/src/main/java/com/microservico/produtoservice/infrastructure/controller/ProdutoController.java
@@ -5,9 +5,9 @@ import com.microservico.produtoservice.domain.model.Produto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/produtos")
@@ -16,11 +16,15 @@ public class ProdutoController {
     @Autowired
     private ProdutoService produtoService;
 
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Produto criarProduto(@RequestBody Produto produto) {
-        return produtoService.criarProduto(produto);
+        Produto produtoCriado = produtoService.criarProduto(produto);
+        System.out.println("📦ETAPA 02 - Produto cadastrado com sucesso: " + produtoCriado.getNome());
+        return produtoCriado;
     }
+
 
     @GetMapping("/{id}")
     public Produto buscarPorId(@PathVariable String id) {
@@ -39,7 +43,9 @@ public class ProdutoController {
     }
 
     @GetMapping("/sku/{sku}")
-    public Optional<Produto> buscarPorSku(@PathVariable String sku) {
-        return produtoService.porSku(sku);
+    public Produto buscarPorSku(@PathVariable String sku) {
+        return produtoService.porSku(sku)
+                .orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND, "Produto com SKU " + sku + " não encontrado"));
     }
 }
